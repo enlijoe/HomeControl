165b1a62cc158f85c7676b6af25f75f6a383b2b5
Leland Moy <Leland.d.moy@gmail.com>
2022-07-24 00:32:28 -0500

    Private Fields for Entities
    Added Private Fields to each of the entites.
    
    Unsure about the meters fields.

--- Server/src/main/java/com/jgr/home/control/entity/meters.java
+++ Server/src/main/java/com/jgr/home/control/entity/meters.java
@@ -1,5 +1,6 @@
 package com.jgr.home.control.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,4 +13,14 @@ public class meters {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	@Column(name = "energy_exported")
+	private long energy_exported;
+	
+	@Column(name = "energy_imported")
+	private long energy_imported;
+	
+	// figure out type
+//	@Column(name = "last_phase_energy_communication_time")
+//	private time last_phase_energy_communication_time;
 }
--- Server/src/main/java/com/jgr/home/control/entity/systemStatus.java
+++ Server/src/main/java/com/jgr/home/control/entity/systemStatus.java
@@ -1,5 +1,6 @@
 package com.jgr.home.control.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,4 +13,17 @@ public class systemStatus {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	@Column(name = "nominal_full_pack_energy")
+	private long nominal_full_pack_energy;
+	
+	@Column(name = "nominal_energy_remaining")
+	private long nominal_energy_remaining;
+	
+	@Column(name = "system_island_state")
+	private String system_island_state;
+	
+	// figure out type
+//	@Column(name = "last_toggle_timestamp")
+//	private Date last_toggle_timestamp;
 }
