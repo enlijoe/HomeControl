f51a3d698b081c88b348228548b80563620703ac
Leland Moy <Leland.d.moy@gmail.com>
2022-07-24 11:56:13 -0500

    Completed entites
    Finished up the fields in the entites.
    
    Added Getters and Setters, and the Constructors

--- Server/src/main/java/com/jgr/home/control/entity/meters.java
+++ Server/src/main/java/com/jgr/home/control/entity/meters.java
@@ -1,5 +1,7 @@
 package com.jgr.home.control.entity;
 
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,9 +12,15 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "meters")
 public class meters {
+	//
+	// private fields
+	//
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	@Column(name = "name", length = 10)
+	private String name;
 	
 	@Column(name = "energy_exported")
 	private long energy_exported;
@@ -20,7 +28,70 @@ public class meters {
 	@Column(name = "energy_imported")
 	private long energy_imported;
 	
-	// figure out type
-//	@Column(name = "last_phase_energy_communication_time")
-//	private time last_phase_energy_communication_time;
+	@Column(name = "last_phase_energy_communication_time")
+	private Date last_phase_energy_communication_time;
+	
+	
+	//
+	// Default Constructor
+	//
+	public meters() {
+		
+	}
+	
+	//
+	// Parameterized Constructor
+	//
+	public meters(String name, long energy_exported, long energy_imported, Date last_phase_energy_communication_time) {
+		super();
+		this.name = name;
+		this.energy_exported = energy_exported;
+		this.energy_imported = energy_imported;
+		this.last_phase_energy_communication_time = last_phase_energy_communication_time;
+	}
+	
+	
+	//
+	// Getters and Setters
+	//
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getEnergy_exported() {
+		return energy_exported;
+	}
+
+	public void setEnergy_exported(long energy_exported) {
+		this.energy_exported = energy_exported;
+	}
+
+	public long getEnergy_imported() {
+		return energy_imported;
+	}
+
+	public void setEnergy_imported(long energy_imported) {
+		this.energy_imported = energy_imported;
+	}
+
+	public Date getLast_phase_energy_communication_time() {
+		return last_phase_energy_communication_time;
+	}
+
+	public void setLast_phase_energy_communication_time(Date last_phase_energy_communication_time) {
+		this.last_phase_energy_communication_time = last_phase_energy_communication_time;
+	}
+	
 }
--- Server/src/main/java/com/jgr/home/control/entity/systemStatus.java
+++ Server/src/main/java/com/jgr/home/control/entity/systemStatus.java
@@ -1,5 +1,7 @@
 package com.jgr.home.control.entity;
 
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,9 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "systemStatus")
 public class systemStatus {
+	//
+	// private fields
+	//
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
@@ -20,10 +25,73 @@ public class systemStatus {
 	@Column(name = "nominal_energy_remaining")
 	private long nominal_energy_remaining;
 	
-	@Column(name = "system_island_state")
+	@Column(name = "system_island_state", length = 30)
 	private String system_island_state;
 	
-	// figure out type
-//	@Column(name = "last_toggle_timestamp")
-//	private Date last_toggle_timestamp;
+	@Column(name = "last_toggle_timestamp")
+	private Date last_toggle_timestamp;
+	
+	//
+	// Default Constructor
+	//
+	public systemStatus() {
+		
+	}
+	
+	//
+	// Parameterized Constructor
+	//
+	public systemStatus(long nominal_full_pack_energy, long nominal_energy_remaining, String system_island_state,
+			Date last_toggle_timestamp) {
+		super();
+		this.nominal_full_pack_energy = nominal_full_pack_energy;
+		this.nominal_energy_remaining = nominal_energy_remaining;
+		this.system_island_state = system_island_state;
+		this.last_toggle_timestamp = last_toggle_timestamp;
+	}
+	
+	
+	//
+	// Getters and Setters
+	//
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getNominal_full_pack_energy() {
+		return nominal_full_pack_energy;
+	}
+
+	public void setNominal_full_pack_energy(long nominal_full_pack_energy) {
+		this.nominal_full_pack_energy = nominal_full_pack_energy;
+	}
+
+	public long getNominal_energy_remaining() {
+		return nominal_energy_remaining;
+	}
+
+	public void setNominal_energy_remaining(long nominal_energy_remaining) {
+		this.nominal_energy_remaining = nominal_energy_remaining;
+	}
+
+	public String getSystem_island_state() {
+		return system_island_state;
+	}
+
+	public void setSystem_island_state(String system_island_state) {
+		this.system_island_state = system_island_state;
+	}
+
+	public Date getLast_toggle_timestamp() {
+		return last_toggle_timestamp;
+	}
+
+	public void setLast_toggle_timestamp(Date last_toggle_timestamp) {
+		this.last_toggle_timestamp = last_toggle_timestamp;
+	}
+	
 }
